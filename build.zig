8dff48625bf99f8c2bf11bc19039a47d407e83b4
Matheus Catarino <matheus-catarino@hotmail.com>
2024-11-17 13:41:16 -0300

    mingw whisper dll path

--- build.zig
+++ build.zig
@@ -62,8 +62,8 @@ pub fn build(b: *std.Build) !void {
             else => "Release",
         }})));
     } else if (exe.rootModuleTarget().isMinGW()) {
-        exe.addLibraryPath(b.path(".zig-cache/whisper_build/src/bin"));
-        exe.addLibraryPath(b.path(".zig-cache/whisper_build/ggml/src/bin"));
+        exe.addLibraryPath(b.path(".zig-cache/whisper_build/bin"));
+        exe.addLibraryPath(b.path(".zig-cache/whisper_build/ggml/bin"));
         exe.addLibraryPath(b.path(".zig-cache/libsndfile"));
     } else {
         exe.addLibraryPath(b.path(".zig-cache/whisper_build/src"));
